580f98d653d43a8a979320cfed1482faef67e1c0
vikatiurne <viktoriya83-08@ukr.net>
2023-04-20 15:41:11 +0300

    added autorization page

--- src/componets/UI/Button/Button.jsx
+++ src/componets/UI/Button/Button.jsx
@@ -1,12 +1,12 @@
 import styles from './Button.module.css';
 
 const Button = (props) => {
-  const { children, type, onclick } = props;
+  const { children, type, onclick,valid } = props;
 
   const classes = [styles.button, styles[type]];
 
   return (
-    <button onClick={onclick} className={classes.join(' ')}>
+    <button onClick={onclick} className={classes.join(' ')} disabled={!valid?true:false}>
       {children}
     </button>
   );
--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,8 +1,89 @@
+import { RxEyeOpen, RxEyeClosed } from 'react-icons/rx';
+import { useState } from 'react';
+import Button from '../../componets/UI/Button/Button';
+import Input from '../../componets/UI/Input/Input';
+
+import styles from './Auth.module.css';
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+  const [inputName, setInputName] = useState('');
+  const [inputPass, setInputPass] = useState('');
+  const [tachedName, setTachedName] = useState(false);
+  const [tachedPass, setTachedPass] = useState(false);
+  const [eye, setEye] = useState(true);
+  const [inputType, setInputType] = useState('password');
 
-export default Auth
+  const handlerInputName = (e) => {
+    setTachedName(true);
+    setInputName(e.target.value);
+  };
+  const handlerInputPass = (e) => {
+    setTachedPass(true);
+    setInputPass(e.target.value);
+  };
+
+  const openEyeHandler = () => {
+    setEye((prev) => !prev);
+    if (inputType === 'password') setInputType('text');
+  };
+  const closeEyeHandler = () => {
+    setEye((prev) => !prev);
+    if (inputType === 'text') setInputType('password');
+  };
+
+  const authHandler = () => {};
+
+  return (
+    <div className={styles.auth}>
+      <p>
+        Введіть ім'я та пароль для входу
+        <br />
+        <span>або пройдіть реєстрацію</span>
+      </p>
+      <form>
+        <Input
+          inputType="text"
+          onChangeInput={handlerInputName}
+          placeholder="Введіть ім'я"
+          value={inputName}
+          autocomplete="username"
+          inputLabel="Ім'я"
+          errorMessage="Ім'я має містити мінімум 8 символів"
+          inputMessage="Ім'я має містити мінімум 8 символів"
+          valid={inputName.trim().length >= 8}
+          tached={tachedName}
+        />
+        <Input
+          inputType={inputType}
+          onChangeInput={handlerInputPass}
+          placeholder="Введіть пароль"
+          value={inputPass}
+          autocomplete="current-password"
+          inputLabel="Пароль"
+          errorMessage="Введіть коректний пароль"
+          inputMessage="Пароль має містити мінімум 8 символів"
+          valid={inputPass.trim().length >= 8}
+          tached={tachedPass}
+        />
+        <RxEyeClosed
+          onClick={openEyeHandler}
+          className={eye ? styles.visible : styles.hide}
+        />
+        <RxEyeOpen
+          onClick={closeEyeHandler}
+          className={eye ? styles.hide : styles.visible}
+        />
+        <div className={styles.authActive}>
+          <Button type="success" valid={inputPass.trim().length >= 8}>
+            Вхід
+          </Button>
+          <Button onclick={authHandler} type="primary" valid={true} title=''>
+            Реєстрація
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
