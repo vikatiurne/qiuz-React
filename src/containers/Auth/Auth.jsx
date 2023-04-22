a15dd68299783f4e05a147f0ba28dd6894f5cfb8
vikatiurne <viktoriya83-08@ukr.net>
2023-04-22 09:30:53 +0300

    auth with firebase

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,5 +1,6 @@
-import { RxEyeOpen, RxEyeClosed } from 'react-icons/rx';
 import { useState } from 'react';
+import axios from 'axios';
+import { RxEyeOpen, RxEyeClosed } from 'react-icons/rx';
 import Button from '../../componets/UI/Button/Button';
 import Input from '../../componets/UI/Input/Input';
 
@@ -31,26 +32,57 @@ const Auth = () => {
     if (inputType === 'text') setInputType('password');
   };
 
-  const authHandler = () => {};
+  const loginHandler = async () => {
+    const authData = { 
+      email: inputName, 
+      password: inputPass,
+      returnSecureToken: true };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAXPJFdxpRxSO6L5VFX0Hdc2gl5RSQQthQ',
+        authData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const authHandler = async () => {
+    const authData = { 
+      email: inputName, 
+      password: inputPass,
+      returnSecureToken: true };
+    try {
+      const response = await axios.post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXPJFdxpRxSO6L5VFX0Hdc2gl5RSQQthQ',
+        authData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className={styles.auth}>
       <p>
-        Введіть ім'я та пароль для входу
+        Введіть e:mail та пароль для входу
         <br />
         <span>або пройдіть реєстрацію</span>
       </p>
-      <form>
+      <form onSubmit={(e)=>e.preventDefault()}>
         <Input
           inputType="text"
           onChangeInput={handlerInputName}
-          placeholder="Введіть ім'я"
+          placeholder="Введіть e-mail"
           value={inputName}
           autocomplete="username"
-          inputLabel="Ім'я"
-          errorMessage="Ім'я має містити мінімум 8 символів"
-          inputMessage="Ім'я має містити мінімум 8 символів"
-          valid={inputName.trim().length >= 8}
+          inputLabel="E:mail"
+          errorMessage="E:mail має містити символ @"
+          inputMessage="E:mail має містити символ @"
+          valid={/^((([0-9A-Za-z]{1}[-0-9A-z.]{0,30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я.]{0,30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}.){1,}[-A-Za-z]{2,})$/gi.test(
+            inputName
+          )}
           tached={tachedName}
         />
         <Input
@@ -74,10 +106,20 @@ const Auth = () => {
           className={eye ? styles.hide : styles.visible}
         />
         <div className={styles.authActive}>
-          <Button type="success" valid={inputPass.trim().length >= 8 && inputName.trim().length >= 8} title='Увійти'>
+          <Button
+          onclick={loginHandler}
+            type="success"
+            valid={inputPass.trim().length >= 8 && inputName.trim().length >= 8}
+            title="Увійти"
+          >
             Вхід
           </Button>
-          <Button onclick={authHandler} type="primary" valid={true} title="Для реєстрації введіть ім'я та пароль">
+          <Button
+            onclick={authHandler}
+            type="primary"
+            valid={true}
+            title="Для реєстрації введіть ім'я та пароль"
+          >
             Реєстрація
           </Button>
         </div>
